1979f6262e3a5a8e8a39e1f40cad335399c0661e
illoquehambre <illoquehambre@gmail.com>
2022-11-23 18:06:23 +0100

    first commit

---
+++ Trianafy/src/main/java/com/Triana/Salesianos/Trianafy/TrianafyApplication.java
@@ -0,0 +1,13 @@
+package com.Triana.Salesianos.Trianafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrianafyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrianafyApplication.class, args);
+	}
+
+}
